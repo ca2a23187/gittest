9cfd2d9ea671699da5177b0cc80dba5a73296ba3
ca2a23187 <2537649358@qq.com>
2020-09-23 09:39:15 +0800

    54365

---
+++ Project1/main.cpp
@@ -0,0 +1,22 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
+int main()
+{
+    cv::Mat srcMat = imread("D:/1.jpg");
+	int height = srcMat.rows;
+	int width = srcMat.cols;
+	for (int j = 0; j < height; j++)
+	{
+		for (int i = 0; i < width;i++)
+		{
+			uchar average = (srcMat.at<Vec3b>(j, i)[0] + srcMat.at<Vec3b>(j, i)[1]+ srcMat.at<Vec3b>(j, i)[2]) / 3;
+			srcMat.at<Vec3b>(j, i)[0] = average;
+			srcMat.at<Vec3b>(j, i)[1] = average;
+			srcMat.at<Vec3b>(j, i)[2] = average;
+		}
+	}
+	imshow("src", srcMat);//等待用户按键
+	waitKey(0);
+	return 0;
+}
