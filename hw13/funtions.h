ae1e828570369228196015d8d7c5b005bec68297
ca2a23187 <2537649358@qq.com>
2020-12-30 10:05:41 +0800

    homework14

---
+++ hw13/funtions.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+VideoCapture createInput(bool useCamera, std::string videoPath);
+
+
+
+void segColor();
+int kMeansDemo();
+
+int createMaskByKmeans(cv::Mat src, cv::Mat &mask);
